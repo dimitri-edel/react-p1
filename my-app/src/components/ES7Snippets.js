2b16e878dc73389b5a24b1302177129069d1b924
dimitri-edel <>
2023-07-17 09:21:39 +0000

    Add extension ES7Snippet and add ES7Snippets.js to componenets to demonstrate the code snippets

---
+++ my-app/src/components/ES7Snippets.js
@@ -0,0 +1,48 @@
+/* rfce Tab - Snippet: import react, function body, export default */
+// import React from 'react'
+
+// function ES7Snippets() {
+//   return (
+//     <div>ES7Snippets</div>
+//   )
+// }
+
+// export default ES7Snippets
+
+/* rafce Tab - Snippet:  import react, const Name =()=> {body}, export default */
+// import React from 'react'
+
+// const ES7Snippets = () => {
+//   return (
+//     <div>ES7Snippets</div>
+//   )
+// }
+
+// export default ES7Snippets
+
+/* rce Tab - Snippet: import react, create and export a class Component  */
+// import React, { Component } from 'react'
+
+// export class ES7Snippets extends Component {
+//   render() {
+//     return (
+//       <div>ES7Snippets</div>
+//     )
+//   }
+// }
+
+// export default ES7Snippets
+
+/* rconst Tab - Snippet: creates a constructor */
+// constructor(props) {
+//   super(props)
+
+//   this.state = {
+//      first
+//   }
+// }
+
+// clg Tab Snippet: console.log
+// console.log(first)
+// clo log with object
+// console.log('first', first)
